59440c264e47df9b8681179dfab76d6f7e59af66
anushka <anushka.123cs@gmail.com>
2025-06-25 10:28:37 +0530

    new updates

--- com.FrameWorkOfSelenium/src/test/java/pack1/ContactTest.java
+++ com.FrameWorkOfSelenium/src/test/java/pack1/ContactTest.java
@@ -6,6 +6,16 @@ public class ContactTest {
 	@Test
 	public void createContactTest() {
 		System.out.println("contact test");
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("execute create contact tests");
+
 	}
 	@Test
 	public void modifyContactTest() {
